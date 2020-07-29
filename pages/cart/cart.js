62771369949ca6799003b1f253b03e0a3c5e8d71
jixufang <2577487706@qq.com>
2020-07-29 18:01:07 +0800

    729

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,13 +1,29 @@
 // pages/cart/cart.js
+import {
+  getSetting,
+  chooseAddress,
+  openSetting
+} from "../../utils/asyncWX.js"
+
+//引用es7 的语法引用，或者勾选增强编译
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    address: {}
   },
+  onShow: function () {
+    const address = wx.getStorageSync("address");
+    this.setData({
+      address: address
+    })
+    if (address != null) {
 
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -15,13 +31,41 @@ Page({
 
   },
   //点击收货地址
-  handleChooseAdress() {
-    wx.chooseAddress({
-      success: (result) => {
-        console.log(result)
-      },
-      fail: () => {},
-      complete: () => {}
-    });
+  async handleChooseAdress() {
+    // wx.getSetting({
+    //   success: (result) => {
+    //     const scopeAddress = result.authSetting["scope.address"];
+    //     if (scopeAddress === true || scopeAddress === undefined) {
+    //       wx.chooseAddress({
+    //         success: (result1) => {
+    //           console.log(result1)
+    //         }
+    //       });
+    //     } else {
+    //       wx.openSetting({
+    //         success: (result3) => {
+    //           console.log(result3)
+    //         },
+    //         fail: () => {},
+    //         complete: () => {}
+    //       });
+    //     }
+    //     console.log(result)
+    //   }
+    // });
+    try {
+      //1获取权限状态
+      const res1 = await getSetting();
+      const scopeAddress = res1.authSetting["scope.address"];
+      //2判断权限状态
+      if (scopeAddress === false) {
+        await openSetting();
+      }
+      const res2 = await chooseAddress();
+      wx.setStorageSync("address", res2);
+      console.log(res2);
+    } catch (error) {
+      console.log(error)
+    }
   }
 })
